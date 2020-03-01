ebc7c78bfe59b0317a26bfbc009863a3d6201daa
Colin Alexander <colintalex@gmail.com>
2020-03-01 01:21:26 -0700

    In progress. Adds instance vars.

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,10 +1,18 @@
-class Enigma
-  attr_reader :chars
+require_relative 'module/timeable'
+
+class Enigma < CipherEngine
+  include Timeable
+
+  attr_reader :keys, :offets, :date
   def initialize
-    @chars = []
+    @keys = 0
+    @offets = 0
+    @date = todays_date_ddmmyy
+    super
   end
 
 
-  def encrypt(message, key_code = 00000, date = 0)
+  def encrypt(message, key_code = 00000, date = @date)
+
   end
 end
