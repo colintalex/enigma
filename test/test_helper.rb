abb959e20c583177061d4ad35829666ec71722c8
Colin Alexander <colintalex@gmail.com>
2020-03-01 00:51:35 -0700

    Adds Mocha

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,4 +2,8 @@ require 'Simplecov'
 SimpleCov.start
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'mocha/minitest'
+
+require './lib/cipher_engine'
 require './lib/enigma'
+require './lib/module/timeable'
