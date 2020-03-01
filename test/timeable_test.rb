b5faec883607570322056582c89817c2907f3103
Colin Alexander <colintalex@gmail.com>
2020-03-01 01:19:36 -0700

    Rename TimeDateTest to TimeableTest

--- test/timeable_test.rb
+++ test/timeable_test.rb
@@ -1,7 +1,7 @@
 require './test/test_helper'
 
 class TimeDateTest < Minitest::Test
-  include TimeDate
+  include Timeable
 
     def test_module_can_return_todays_date
       Time.stubs(:now).returns([1, 1, 1, 6, 3, 2017, 1, 1])
