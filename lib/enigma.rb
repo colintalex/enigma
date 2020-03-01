ddbcd738428cc629dadbd0ddb413d4245a87f720
Colin Alexander <colintalex@gmail.com>
2020-03-01 02:51:14 -0700

    Adds assign_keys and assign_offsets. Tested and passing.

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,16 +3,34 @@ require_relative 'module/timeable'
 class Enigma < CipherEngine
   include Timeable
 
-  attr_reader :keys, :offets, :date
+  attr_reader :keys, :offsets, :date
   def initialize
-    @keys = 0
-    @offets = 0
+    @keys = {}
+    @offsets = {}
     @date = todays_date_ddmmyy
+    @shifts = {:A_shift => [], :B_shift => [], :C_shift => [], :D_shift => [],}
     super
   end
 
 
-  def encrypt(message, key_code = 00000, date = @date)
+  def encrypt(message, key_code = create_random_num, date = @date)
+    assign_keys(key_code)
+    date_code(date)
+  end
 
+  def assign_keys(key_code)
+    split_code = key_code.chars.map { |char| char.to_i}
+    @keys[:A] = split_code[0] + split_code[1]
+    @keys[:B] = split_code[1] + split_code[2]
+    @keys[:C] = split_code[2] + split_code[3]
+    @keys[:D] = split_code[3] + split_code[4]
+  end
+
+  def assign_offsets(date)
+    num_date = ((date.to_i)**2).digits.reverse
+    @offsets[:D] = num_date.pop
+    @offsets[:C] = num_date.pop
+    @offsets[:B] = num_date.pop
+    @offsets[:A] = num_date.pop
   end
 end
