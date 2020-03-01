60fab7a4a3cac74a6bde6ac1e216d0c62bdf5ee6
Colin Alexander <colintalex@gmail.com>
2020-03-01 00:48:03 -0700

    Creates Test class for date_time module

---
+++ test/time_date_test.rb
@@ -0,0 +1,10 @@
+require './test/test_helper'
+
+class TimeDateTest < Minitest::Test
+  include TimeDate
+
+    def test_module_can_return_todays_date
+      Time.stubs(:now).returns([1, 1, 1, 6, 3, 2017, 1, 1])
+      assert_equal "060317", todays_date_ddmmyy
+    end
+end
