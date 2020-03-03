c763bf5a9629d486dc1093da1052f7219b9f213a
Colin Alexander <colintalex@gmail.com>
2020-03-02 21:54:31 -0700

    Require_relative to require for test files

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,4 +1,4 @@
-require_relative 'test_helper'
+require './test/test_helper'
 
 class EnigmaTest < Minitest::Test
   include Timeable
--- test/timeable_test.rb
+++ test/timeable_test.rb
@@ -1,4 +1,4 @@
-require_relative 'test_helper'
+require './test/test_helper'
 
 class TimeDateTest < Minitest::Test
   include Timeable
